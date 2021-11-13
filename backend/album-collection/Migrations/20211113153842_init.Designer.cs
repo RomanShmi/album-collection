6a807084eff19d840d3eb615d5ebf6dbb8d4a0d0
roman <romyli4ka@gmail.com>
2021-11-13 10:40:36 -0500

    corected database

--- backend/album-collection/Migrations/20211113153842_init.Designer.cs
+++ backend/album-collection/Migrations/20211113153842_init.Designer.cs
@@ -9,15 +9,15 @@ using album_collection;
 namespace album_collection.Migrations
 {
     [DbContext(typeof(AlbumsContext))]
-    [Migration("20211110013144_initial")]
-    partial class initial
+    [Migration("20211113153842_init")]
+    partial class init
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
-                .HasAnnotation("ProductVersion", "5.0.7")
+                .HasAnnotation("ProductVersion", "5.0.12")
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
             modelBuilder.Entity("album_collection.Models.Album", b =>
--- backend/album-collection/Migrations/AlbumsContextModelSnapshot.cs
+++ backend/album-collection/Migrations/AlbumsContextModelSnapshot.cs
@@ -15,7 +15,7 @@ namespace album_collection.Migrations
 #pragma warning disable 612, 618
             modelBuilder
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
-                .HasAnnotation("ProductVersion", "5.0.7")
+                .HasAnnotation("ProductVersion", "5.0.12")
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
             modelBuilder.Entity("album_collection.Models.Album", b =>
